3ceaaab552601090d208fa3585f8e19e53911dff
Theodor <95082046+TheodorSTTH@users.noreply.github.com>
2023-04-30 08:58:41 +0200

    last

--- components/CollectionPreview.jsx
+++ components/CollectionPreview.jsx
@@ -34,7 +34,7 @@ export default function CollectionPreview({collectionId}) {
             <p>{about}</p>
             <p>{cards} card{cards !== 1 && "s"}</p>
             <div className="card-actions justify-end flex gap-4">
-                <a href={"https://twitter.com/intent/tweet?url=" + collectionId}>
+                <a href={"https://twitter.com/intent/tweet?url=https://chatcards.vercel.app/playcollection/" + collectionId}>
                     <button className="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></svg>
                     </button>
